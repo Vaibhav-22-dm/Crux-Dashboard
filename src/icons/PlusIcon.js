0a8ea09865b0ade9b3088957cee5dfdc9796eea7
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2024-02-24 12:54:10 +0530

    Updates

--- src/icons/PlusIcon.js
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-import { SvgIcon } from "@mui/material";
-
-export default function PlusIcon(props) {
-    return (<h1 style={{ fontSize: "20px", fontWeight: "200", marginBottom: "5px" }}>+</h1>
-    );
-}
