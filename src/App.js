b5404cc0b85cbf7d3b6bd7d92d6392cee6a830f6
Vaibhav Mohite <vaibhavdmohite22@gmail.com>
2024-02-24 14:10:45 +0530

    add: basename in router

--- src/App.js
+++ src/App.js
@@ -38,9 +38,8 @@ function App() {
 
   return (
     <div className="App">
-      <div>Hello</div>
       <ThemeProvider theme={theme}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
           <Routes>
             <Route path="/" element={
               <>
